0008f7434c1d39d015268f14394d412e7e763385
jaturtur <kotijahturahmah@mhs.politala.ac.id>
2025-01-06 12:17:07 +0800

    berhasil

---
+++ app/Controllers/Admin.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\ModelAdmin;
+
+class Admin extends BaseController
+{
+
+    public function __construct()
+    {
+        $this->ModelAdmin = new ModelAdmin();
+    }
+
+    public function index()
+    {
+        $data = [
+            'judul' => 'Dashboard',
+            'menu' => 'dashboard',
+            'page'  => 'backend/v_dashboard',
+        ];
+        return view('v_template_back', $data);
+    }
+    public function setting()
+   {
+    $data = [
+        'judul' => 'Setting',
+        'menu' => 'setting',
+        'page' => 'backend/v_setting',
+        'setting' => $this->ModelAdmin->dataSetting(),
+    ];
+    return view('v_template_back', $data);
+    }
+    public function updateSetting()
+   {
+    $data = [
+        'nama_kantor' => $this->request->getPost('nama_kantor'),
+        'alamat' => $this->request->getPost('alamat'),
+        'lokasi_kantor' => $this->request->getPost('lokasi_kantor'),
+    ];
+    $this->ModelAdmin->updateSetting($data);
+    session()->setFlashdata('pesan', 'Data Berhasil Diupdate !!!');
+    return redirect()->to('Admin/setting');
+   }
+}
---
+++ app/Controllers/Presensi.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\ModelPresensi;
+
+class Presensi extends BaseController
+{
+    public function __construct()
+    {
+     $this->ModelPresensi = new ModelPresensi();
+    }
+    public function index()
+    {
+    $presensi = $this -> ModelPresensi -> cekPresensi();
+    if ($presensi == null) {
+        //buka absen masuk
+        $data = [
+            'judul' => 'Absen Masuk',
+            'menu' => 'presensi',
+            'page' => 'presensi/v_absen_masuk',
+        ];
+        return view('v_template_front', $data);
+    } else {
+        $data = [
+            'judul' => 'Absen Pulang',
+            'menu' => 'presensi',
+            'page' => 'presensi/v_absen_pulang',
+        ];
+        return view('v_template_front', $data);
+    }
+    }
+}
